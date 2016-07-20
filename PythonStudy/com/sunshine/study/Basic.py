9e9a5c3b2eba180276fa433ccad5d7d059e5c478
SunshineDreams <mysunshinedreams@163.com>
2016-07-20 12:13:10 +0800

    修正语法错误

--- PythonStudy/com/sunshine/study/Basic.py
+++ PythonStudy/com/sunshine/study/Basic.py
@@ -9,11 +9,11 @@
 print('hello world')
 
 # print()函数也可以接受多个字符串
-print ('I', 'am', 'a', 'coder')
+print('I', 'am', 'a', 'coder')
 
 # 输入
 name = input()
-name
+
 print(name)
 
 # 条件语句
@@ -21,9 +21,7 @@ age = 22
 
 if 0 < age <= 18:
     print('未成年')
-else if 18 < age <= 28 : 
+elif 18 < age <= 28:
     print('青年')
-else if 28 < age <= 60 :
+elif 28 < age <= 60:
     print('中年')
-else if age > 60 :
-    print('老年')
